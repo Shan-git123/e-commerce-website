2ea2c19e3c147483d819c247a7e2d724200ee14c
Shahin Qureshi <shahin2171993@gmail.com>
2023-03-02 15:44:42 +0530

    fixed css

--- src/Product.js
+++ src/Product.js
@@ -2,11 +2,22 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ShopContext } from "./ShopContextProvider";
 
+import { addDoc , collection } from "firebase/firestore";
+import { db } from "./firebase-config";
+
+const cartProductRef = collection(db, "savedCartItems");
+
 const Product = ({ data }) => {
   const { addToCart, cartItems } = useContext(ShopContext);
   const { id, productName, price, img } = data;
   const cartItemAmount = cartItems[id];
   const navigate = useNavigate();
+
+  const savedInCartDatabase =  async() =>{
+    addToCart(id);
+    await addDoc(cartProductRef, { cartProductName :productName } )
+
+  }
 
   return (
     <div className="product">
@@ -23,7 +34,7 @@ const Product = ({ data }) => {
           Go To Cart
         </button>
       ) : (
-        <button className="addToCartBtn" onClick={() => addToCart(id)}>
+        <button className="addToCartBtn" onClick={() => savedInCartDatabase()}>
           Add to cart
         </button>
       )}
